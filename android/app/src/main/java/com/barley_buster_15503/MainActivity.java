c97b58c61ff90bb9c1a3a033aad5587c35dbd333
Crowdbotics <team@crowdbotics.com>
2020-04-06 13:22:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/barley_buster_15503/MainActivity.java
@@ -0,0 +1,15 @@
+package com.barley_buster_15503;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "barley_buster_15503";
+    }
+}
